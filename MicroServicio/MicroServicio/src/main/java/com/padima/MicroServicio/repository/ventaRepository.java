57bdce3ec86f618218c74e4a016689aafc243cba
reyesgb <reyesebastian03@gmail.com>
2025-05-24 05:59:04 -0400

    Delete MicroServicio/MicroServicio directory

--- MicroServicio/MicroServicio/src/main/java/com/padima/MicroServicio/repository/ventaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padima.MicroServicio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.padima.MicroServicio.model.venta;
-
-public interface ventaRepository extends JpaRepository<venta,Long>{
-
-}
